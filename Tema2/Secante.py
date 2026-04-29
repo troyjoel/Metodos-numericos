491c1460267838e090403d98c728621fd5a413f1
troyjoel <ftroyjoel@gmail.com>
2026-04-29 09:19:54 -0600

    Update Secante.py

--- Tema2/Secante.py
+++ Tema2/Secante.py
@@ -1,1 +1,17 @@
+def f(x):
+    return x**2 - 4  # cambia función
 
+x0 = float(input("Ingresa x0: "))
+x1 = float(input("Ingresa x1: "))
+tol = float(input("Tolerancia: "))
+
+while True:
+    x2 = x1 - (f(x1)*(x1 - x0)) / (f(x1) - f(x0))
+
+    if abs(x2 - x1) < tol:
+        break
+
+    x0 = x1
+    x1 = x2
+
+print("Raíz aproximada:", x2)
